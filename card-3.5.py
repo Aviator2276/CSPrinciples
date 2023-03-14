b75eb876f9d855484fee28b028dcbbe22aec20dd
aviator2276 <aviator2276@gmail.com>
2023-03-14 13:19:04 +0000

    Finished card 3.5 module

--- card-3.5.py
+++ card-3.5.py
@@ -1,13 +1,54 @@
-cardRank = input(str("Input the Card Rank: "))
-cardSuit = input(str("Input the Card Suit: "))
-cardColor = input(str("Input the Card Color: "))
+# START
+# ----- USER INPUT -----
+# Get the users card information to be converted to binary
+cardRank = input(str('Input the Card Rank: '))
+cardSuit = input(str('Input the Card Suit: '))
+cardColor = input(str('Input the Card Color: '))
 
+# Convert all of their answers to an easy lowercase variable
+cardRank = cardRank.lower()
+cardSuit = cardSuit.lower()
 cardColor = cardColor.lower()
 
-if cardColor == "red":
+# ----- VARIABLES -----
+# Set all our variables before we start the program
+# 'rank' sets the predefined variables to compare to
+rank = ['ace','2','3','4','5','6','7','8','9','10','jack','queen','king']
+# 'rankCode' sets the predefinced variables that we change to
+rankCode = ['0000','0001','0010','0100','1000','1001','1010','1100','1101','1110','1111','0110','0101']
+# 'suit' sets the predefinced variables to compare to
+suit = ['spades','clubs','hearts','diamonds']
+# 'suiteCode' sets the predefinced variables that we change to
+suitCode = ['00','01','10','11']
+# 'countVar' is our counter variable in the FOR LOOP
+countVar = 0
+# These preset the final variables as empty
+rankBinary = ""
+suitBinary = ""
+
+# ----- COLOR -----
+# This sets the color of our card to a 1 bit binary code
+if cardColor == 'red':
     cardColor = 1
-elif cardColor == "black":
+elif cardColor == 'black':
     cardColor = 0
-print(str(cardColor))
-print("Hello World")
-    
+
+# ----- RANK ------
+# This sets the rank of our card to a 4 bit binary code
+for x in rank:
+    if str(cardRank) == str(x):
+        rankBinary = rankCode[countVar]
+    countVar = countVar + 1
+
+# ----- SUIT -----
+# This sets the rank of our card to a 2 bit binary code
+countVar = 0
+for x in suit:
+    if str(cardSuit) == str(x):
+        suitBinary = suitCode[countVar]
+    countVar = countVar + 1
+
+# ----- OUTPUT -----
+# Once all is done, print out the converted card in binary code
+print(str(rankBinary)+str(suitBinary)+str(cardColor))
+# END
